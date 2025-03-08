d9918602f87fc8c953a19041cc9efa07721ca987
Kirill <kurumi8927@gmail.com>
2025-03-09 00:43:42 +0300

    починил

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Route, Routes } from "react-router-dom";
 import { Main } from "./components/Main/Main";
 
 import { Comments } from "./components/Comments/Comments";
+import { Login } from "./components/Login/Login";
+import { NotFound } from "./components/NotFound/NotFound";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Main />} />
-      <Route path="/login" />
+      <Route path="/login" element={<Login />} />
       <Route path="/comment" element={<Comments />} />
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,6 +1,37 @@
-//@ts-ignore
-import s from "./Login.module.css";
+import { Header } from "../Header/Header";
+import styles from "./Login.module.css";
 
-export const Login = () => {
-  return <section className={s.login}></section>;
-};
+export function Login() {
+  return (
+    <div className={styles.loginContainer}>
+      <Header />
+      <div className={styles.loginBox}>
+        <h2 className={styles.loginTitle}>Вход</h2>
+
+        <div className={styles.inputGroup}>
+          <label>Email</label>
+          <input
+            type="email"
+            className={styles.inputField}
+            placeholder="Введите email"
+          />
+        </div>
+
+        <div className={styles.inputGroup}>
+          <label>Пароль</label>
+          <input
+            type="password"
+            className={styles.inputField}
+            placeholder="Введите пароль"
+          />
+        </div>
+
+        <button className={styles.loginButton}>Войти</button>
+
+        <p className={styles.registerLink}>
+          Нет аккаунта? <a href="#">Зарегистрироваться</a>
+        </p>
+      </div>
+    </div>
+  );
+}
