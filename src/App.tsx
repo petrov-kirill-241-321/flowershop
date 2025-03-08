fe11e3a6b0872aa648cbfb2a98eb55008ee62bee
SVArtem <artem.schcerbina@mail.ru>
2025-03-09 00:10:46 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,15 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import { Main } from "./components/Main/Main";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import NotFound from './NotFound';
+import Home from './Home'; // Ваш компонент главной страницы
 
-import { Comments } from "./components/Comments/Comments";
-
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Main />} />
-      <Route path="/login" />
-      <Route path="/comment" element={<Comments />} />
-    </Routes>
-  );
-}
-
-export default App;
+const App = () => {
+    return (
+        <Router>
+            <Switch>
+                <Route path="/login" component={Login} />
+                <Route path="/404" component={NotFound} />
+                <Route path="/home" component={Home} />
+                <Route path="/" exact component={Login} /> {/* По умолчанию перенаправляем на страницу логина */}
+                <Route component={NotFound} /> {/* Перенаправление на 404 для несуществующих маршрутов */}
